3faaf60803b7106eb6dbd46357fa898972f267cb
Nitish Kumar <nitish7290951173@gmail.com>
2025-02-20 16:13:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI(
     description="Predicting Water Potability"
 )
 
-with open(r"C:\Users\Nitish\ML_Pipeline\model2.pkl","rb") as f:
+with open(r"model2.pkl","rb") as f:
     model = pickle.load(f)
 
 @app.get("/")
